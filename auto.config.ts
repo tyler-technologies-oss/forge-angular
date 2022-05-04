6d0660029dae81c3adc96dc88dac36b369e648fb
Nichols, Kieran <kieran.nichols@tylertech.com>
2022-05-04 16:16:43 -0400

    chore(release): v2.0.0-rc.0 [skip ci]

--- auto.config.ts
+++ auto.config.ts
@@ -1,14 +1,9 @@
 import { AutoRc } from 'auto';
 import { INpmConfig } from '@auto-it/npm';
-import { IExecPluginOptions } from '@auto-it/exec';
 
 const npmOptions: INpmConfig = {
   setRcToken: false,
   publishFolder: 'dist/forge-angular'
-};
-
-const execOptions: IExecPluginOptions = {
-  afterVersion: `cd ${npmOptions.publishFolder} && npm version $npm_package_version --no-git-tag-version --no-commit-hooks --ignore-scripts`
 };
 
 export default function rc(): AutoRc {
@@ -18,12 +13,10 @@ export default function rc(): AutoRc {
     author: 'GitHub Actions <41898282+github-actions[bot]@users.noreply.github.com>',
     canary: { target: 'pr-body' },
     plugins: [
-      ['exec', execOptions],
       ['npm', npmOptions],
       'conventional-commits',
       'released',
-      'first-time-contributor',
-      'pr-body-labels'
+      'first-time-contributor'
     ]
   };
 }
