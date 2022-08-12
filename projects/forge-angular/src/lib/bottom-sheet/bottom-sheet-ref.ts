74ef5f05bbddd04e28e7cfff49c1b4a099d973b8
Mike Matuszak <Michael.Matuszak@tylertech.com>
2022-08-12 11:22:03 -0400

    fix(bottom-sheet): wait for close animation (#11)
    Fixing bug on iOS Safari where backdrop gets stuck after closing. Also restores the close animation in all browsers.

--- projects/forge-angular/src/lib/bottom-sheet/bottom-sheet-ref.ts
+++ projects/forge-angular/src/lib/bottom-sheet/bottom-sheet-ref.ts
@@ -1,11 +1,11 @@
 import { ElementRef } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { AsyncSubject, Observable, Subject } from 'rxjs';
 
 import { BOTTOM_SHEET_CONSTANTS, IBottomSheetComponent } from '@tylertech/forge';
 
 export class BottomSheetRef<T = any> {
   private readonly _elementRef: ElementRef<IBottomSheetComponent>;
-  private readonly _afterClosed = new Subject<any>();
+  private readonly _afterClosed = new AsyncSubject<any>();
   public afterClosed: Observable<any> = this._afterClosed.asObservable();
   private readonly _beforeClose = new Subject<CustomEvent>();
   public beforeClose: Observable<CustomEvent> = this._beforeClose.asObservable();
@@ -13,7 +13,7 @@ export class BottomSheetRef<T = any> {
 
   constructor(instance: IBottomSheetComponent) {
     this._elementRef = new ElementRef(instance);
-    instance.addEventListener(BOTTOM_SHEET_CONSTANTS.events['BEFORE_CLOSE'], evt => this._beforeClose.next(evt as CustomEvent));
+    instance.addEventListener(BOTTOM_SHEET_CONSTANTS.events.BEFORE_CLOSE, evt => this._beforeClose.next(evt as CustomEvent));
   }
 
   public close(result?: any): void {
