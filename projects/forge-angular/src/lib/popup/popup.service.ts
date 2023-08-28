200b51a39c0d796ee79b66d19b1d9bca785b7221
Mike Matuszak <Michael.Matuszak@tylertech.com>
2023-08-28 07:53:27 -0400

    feat(popup-service): dynamically render component in forge-popup (#30)

---
+++ projects/forge-angular/src/lib/popup/popup.service.ts
@@ -0,0 +1,98 @@
+import { Injectable, Injector, NgModuleRef, Type } from '@angular/core';
+import { IPopupComponent, POPUP_CONSTANTS, definePopupComponent } from '@tylertech/forge';
+import { DynamicComponentService, IDynamicComponentRef } from '../core/dynamic-component/dynamic-component.service';
+import { PopupConfig } from './popup-config';
+import { PopupInjector } from './popup-injector';
+import { PopupRef } from './popup-ref';
+
+export interface IPopupOptions extends Omit<Partial<IPopupComponent>, 'attributes'> {
+  popupClass?: string;
+  attributes?: Map<string, string>;
+  targetElement: HTMLElement;
+}
+
+/**
+ * Provides facilities for working with a Forge popup and placing dynamic components within it.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class PopupService {
+  constructor(private _dcs: DynamicComponentService, private _injector: Injector) {
+    definePopupComponent();
+  }
+
+  /**
+   * Displays a components within a Forge popup instance.
+   * @param component The component reference.
+   * @param options Sets attributes and properties on the `forge-popup` element. `targetElement` is required.
+   * @param config The configuration to provide to the dynamic component as an injectable token.
+   * @param moduleRef Optional NgModule ref if need by the component.
+   * @returns A PopupRef to the forge-popup and dynamically created component, and related close handling.
+   */
+  public show<T, TModule>(
+    component: Type<T>,
+    options: IPopupOptions,
+    config?: PopupConfig,
+    moduleRef?: NgModuleRef<TModule>
+  ): PopupRef<T> {
+    // Contains tokens that will be provided to components through our custom popup injector
+    const map = new WeakMap();
+
+    // If we got a config, we should provide it as an injection token
+    if (config) {
+      map.set(PopupConfig, config);
+    }
+
+    // Create the Forge popup element
+    const popupElement = document.createElement('forge-popup');
+
+    // Configure the popup with the provided options
+    const { popupClass, attributes, ...restOptions } = options;
+    if (popupClass) {
+      popupElement.classList.add(popupClass);
+    }
+    if (attributes) {
+      attributes.forEach((value, key) => popupElement.setAttribute(key, value));
+    }
+    Object.assign(popupElement, restOptions);
+
+    // Create the ref that will allow the consumer to control the popup
+    const popupRef = new PopupRef<T>(popupElement);
+
+    // Always provide the popup ref as an injection token
+    map.set(PopupRef, popupRef);
+
+    // Create and attach the dynamic component to the popup element
+    const dcRef = this._dcs.create(component, popupElement, new PopupInjector(this._injector, map), moduleRef);
+    popupRef.componentInstance = dcRef.componentRef.instance;
+
+    // Always destroy when the popup is closed
+    const sub = popupRef.afterClosed.subscribe(() => {
+      this._destroy(popupElement, dcRef);
+      sub.unsubscribe();
+    });
+
+    // Event is dispatched on the target element, not the popup itself
+    options.targetElement.addEventListener(POPUP_CONSTANTS.events.CLOSE, () => {
+      popupRef.close();
+      this._destroy(popupElement, dcRef);
+      sub.unsubscribe();
+    });
+
+    // Appends the popup element to the DOM
+    popupElement.open = true;
+
+    return popupRef;
+  }
+
+  /**
+   * Removes a popup from the DOM and destroys the component instance.
+   * @param popupInstance An instance of a Forge popup element.
+   * @param ref A reference to the dynamic component.
+   */
+  private _destroy<T>(popupInstance: IPopupComponent, ref: IDynamicComponentRef<T>): void {
+    popupInstance.open = false;
+    ref.destroy();
+  }
+}
--- src/app/views/components/popup/popup.component.ts
+++ src/app/views/components/popup/popup.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { PopupDirective } from '@tylertech/forge-angular';
+import { PopupDirective, PopupService, IPopupOptions, ToastService, PopupRef } from '@tylertech/forge-angular';
+import { ExamplePopupComponent } from './example-popup.component';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html'
 })
 export class PopupComponent {
+  constructor(private _popupService: PopupService, private _toastService: ToastService) { }
+
   public popupTextValue = 'Popup text field';
 
   public popupOpen(popupDirective: PopupDirective): void {
@@ -19,5 +22,42 @@ export class PopupComponent {
 
   public popupClosed(): void {
     console.log('popup closed.');
+  }
+
+  private _popupRef?: PopupRef;
+
+  public async showPopupViaService(targetElement: HTMLElement): Promise<void> {
+    this._popupRef?.close();
+
+    const popupOptions: IPopupOptions = {
+      popupClass: 'test-class',
+      targetElement,
+      attributes: new Map([
+        ['aria-labelledby', 'popup-title'],
+        ['aria-describedby', 'popup-desc']
+      ]),
+      closeCallback: () => {
+        console.log('closeCallback');
+      }
+    };
+
+    const popupConfig = {
+      data: {
+        title: 'Confirm',
+        message: 'Are you sure you want to close the modal?'
+      }
+    };
+
+    this._popupRef = this._popupService.show(
+      ExamplePopupComponent,
+      popupOptions,
+      popupConfig
+    );
+    console.log('Native Forge popup instance', this._popupRef.nativeElement);
+    console.log('[PopupRef] Angular componentInstance', this._popupRef.componentInstance);
+    this._popupRef.afterClosed.subscribe(() => {
+      this._toastService.show(`Popup closed.`);
+      this._popupRef = undefined;
+    });
   }
 }
