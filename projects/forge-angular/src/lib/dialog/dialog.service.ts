5e0bff598afa0442c2a49a75e2e904bb1f115fa4
ScottTylerTech <scott.shirley@tylertech.com>
2024-01-17 11:33:16 -0800

    feat: add dialog reference tracking and unsubscribe functionality

--- projects/forge-angular/src/lib/dialog/dialog.service.ts
+++ projects/forge-angular/src/lib/dialog/dialog.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable, Injector, OnDestroy } from '@angular/core';
 import { Type, ComponentFactory, NgModuleRef } from '@angular/core';
 import { IDialogComponent, DIALOG_CONSTANTS, defineDialogComponent } from '@tylertech/forge';
 import { DialogConfig } from './dialog-config';
@@ -6,6 +6,7 @@ import { DialogRef } from './dialog-ref';
 import { DialogInjector } from './dialog-injector';
 import { DynamicComponentService } from '../core/dynamic-component/dynamic-component.service';
 import { IDynamicComponentRef } from '../core/dynamic-component/dynamic-component.service';
+import { Subject, take, takeUntil } from 'rxjs';
 
 export interface IDialogOptions extends Omit<Partial<IDialogComponent>, 'attributes'> {
   dialogClass?: string;
@@ -18,7 +19,9 @@ export interface IDialogOptions extends Omit<Partial<IDialogComponent>, 'attribu
 @Injectable({
   providedIn: 'root'
 })
-export class DialogService {
+export class DialogService implements OnDestroy {
+  private _dialogRefs: DialogRef[] = [];
+  private _unsubscribe$ = new Subject();
   constructor(private _dcs: DynamicComponentService, private _injector: Injector) {
     defineDialogComponent();
   }
@@ -28,7 +31,19 @@ export class DialogService {
    * @param component The component reference.
    * @param config The configuration to provide to the dynamic component as an injectable token.
    */
-  public show<T, K>(component: Type<T> | ComponentFactory<T>, options?: IDialogOptions, config?: DialogConfig, moduleRef?: NgModuleRef<K>): DialogRef<T> {
+  public show<T, K>(
+    component: Type<T>,
+    options?: IDialogOptions,
+    config?: DialogConfig,
+    moduleRef?: NgModuleRef<K>
+  ): DialogRef<T> {
+    const dialogRef = this._showDialog(component, options, config, moduleRef);
+    this._dialogRefs.push(dialogRef);
+    dialogRef.afterClosed.pipe(take(1), takeUntil(this._unsubscribe$)).subscribe(() => this._removeDialogRef(dialogRef));
+    return dialogRef;
+  }
+
+  private _showDialog<T, K>(component: Type<T> | ComponentFactory<T>, options?: IDialogOptions, config?: DialogConfig, moduleRef?: NgModuleRef<K>): DialogRef<T> {
     // Contains tokens that will be provided to components through our custom dialog injector
     const map = new WeakMap();
 
@@ -91,5 +106,42 @@ export class DialogService {
   private _destroy<T>(dialogInstance: IDialogComponent, ref: IDynamicComponentRef<T>): void {
     dialogInstance.open = false;
     ref.destroy();
+  }
+
+  public ngOnDestroy(): void {
+    this._unsubscribe$.next(null);
+    this._unsubscribe$.complete();
+  }
+
+  /**
+   * Closes all dialogs. Will close up to 2 nested dialogs.
+   * @param result The result of closing the dialogs. Default is false.
+   */
+  public closeAllDialogs(result = false): void {
+    this._closeAllDialogs(result);
+  }
+
+  private _closeAllDialogs(result: boolean, recursiveExecutionCount = 0): void {
+    if (recursiveExecutionCount > 2) {
+      throw new Error('Could not close all dialogs. Reason: Too many nested dialogs.');
+    }
+
+    this._dialogRefs.forEach((ref) => ref.close(result));
+    console.table(this._dialogRefs);
+
+    // This is here to close any dialogs that open as a result of other dialogs closing
+    // e.g. A dirty dialog opening when a dirty form dialog closes.
+    if (this._dialogRefs.length > 0) {
+      this._closeAllDialogs(result, ++recursiveExecutionCount);
+    }
+  }
+
+  private _removeDialogRef(ref: DialogRef): void {
+    const index = this._dialogRefs.findIndex((dlgRef) => ref === dlgRef);
+    if (index < 0) {
+      return;
+    }
+
+    this._dialogRefs.splice(index, 1);
   }
 }
