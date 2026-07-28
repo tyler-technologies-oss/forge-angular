d74cd65b60945ade7547ed12bac6af9d7c7ca8f7
Sam Richardson <sam.richardson@tylertech.com>
2026-07-28 10:40:38 -0400

    feat: update to forge 3.15.2

--- projects/forge-angular/src/lib/timeline/timeline.module.ts
+++ projects/forge-angular/src/lib/timeline/timeline.module.ts
@@ -2,18 +2,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { defineTimelineComponent } from '@tylertech/forge';
+import { ForgeTimelineBreakModule } from '../timeline-break/timeline-break.module';
+import { ForgeTimelineItemModule } from '../timeline-item/timeline-item.module';
 
 import { TimelineComponent } from './timeline.component';
-import { ForgeTimelineBreakModule } from '../timeline-break';
-import { ForgeTimelineItemModule } from '../timeline-item';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ForgeTimelineItemModule, ForgeTimelineBreakModule],
   declarations: [TimelineComponent],
   exports: [
     TimelineComponent,
-    ForgeTimelineBreakModule,
     ForgeTimelineItemModule,
+    ForgeTimelineBreakModule,
   ],
 })
 export class ForgeTimelineModule {
